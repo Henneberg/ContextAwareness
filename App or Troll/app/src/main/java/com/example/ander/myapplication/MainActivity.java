32de18fbab7d94a8404c71f420d6d1ca835769ec
Henneberg <henneberg.anders@gmail.com>
2015-11-30 20:26:20 +0100

    Added app, maybe

---
+++ App or Troll/app/src/main/java/com/example/ander/myapplication/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.ander.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button accelButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        accelButton = (Button) findViewById(R.id.accelButton);
+        accelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent accel = new Intent(getApplicationContext(), AccelerometerActivity.class);
+                startActivity(accel);
+            }
+        });
+
+        // comments
+    }
+}
