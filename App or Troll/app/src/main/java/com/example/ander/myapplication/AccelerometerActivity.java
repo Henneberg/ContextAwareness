6e2bdd6aedc2a2195956972eda2f04e47a032382
Mathias <mathiasveje@gamil.com>
2015-12-01 17:03:16 +0100

    affr implmented

--- App or Troll/app/src/main/java/com/example/ander/myapplication/AccelerometerActivity.java
+++ App or Troll/app/src/main/java/com/example/ander/myapplication/AccelerometerActivity.java
@@ -5,9 +5,20 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.io.File;
+import java.io.IOException;
+
+import weka.core.Attribute;
+import weka.core.FastVector;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.converters.ArffSaver;
 
 public class AccelerometerActivity extends AppCompatActivity implements SensorEventListener {
 
@@ -15,9 +26,15 @@ public class AccelerometerActivity extends AppCompatActivity implements SensorEv
     private Sensor senAccelerometer;
     private long lastUpdate = 0;
     private float last_x, last_y, last_z;
-    private static final int SHAKE_THRESHOLD = 600;
+    private static final int SHAKE_THRESHOLD_LOW = 600;
+    private static final int SHAKE_THRESHOLD_HIGH = 1200;
     TextView stepText;
     private int steps = 0;
+    private int runSteps = 0;
+    FastVector atts;
+    Instances data;
+    double[] vals;
+
 
 
     @Override
@@ -30,12 +47,44 @@ public class AccelerometerActivity extends AppCompatActivity implements SensorEv
         senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
 
         stepText = (TextView) findViewById(R.id.accelTexr);
+        final TextView dataField = (TextView) findViewById(R.id.dataView);
+        Button dataButton = (Button) findViewById(R.id.dataBotton);
+        dataButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dataField.setText(data.toString());
+            }
+        });
+
+        atts = new FastVector();
+        atts.addElement(new Attribute("mode"));
+        atts.addElement(new Attribute("time"));
+        data = new Instances("Accelerometer", atts, 0);
+
+        Button gemData = (Button) findViewById(R.id.gemData);
+        gemData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ArffSaver saver = new ArffSaver();
+                saver.setInstances(data);
+                try {
+                    saver.setFile(new File("../Download/test.arff"));
+                    saver.writeBatch();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
 
     }
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
         Sensor mySensor = sensorEvent.sensor;
+
+
         if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
             float x = sensorEvent.values[0];
             float y = sensorEvent.values[1];
@@ -43,16 +92,17 @@ public class AccelerometerActivity extends AppCompatActivity implements SensorEv
 
             long curTime = System.currentTimeMillis();
 
-            if ((curTime - lastUpdate) > 100) {
+            if ((curTime - lastUpdate) > 1000) {
                 long diffTime = (curTime - lastUpdate);
                 lastUpdate = curTime;
 
                 float speed = Math.abs(x + y + z - last_x - last_y - last_z)/ diffTime * 10000;
-
-                if (speed > SHAKE_THRESHOLD) {
-                    steps++;
-                    stepText.setText(""+steps);
-                }
+                steps++;
+                stepText.setText(""+steps);
+                vals = new double[data.numAttributes()];
+                vals[0] = speed;
+                vals[1] = System.currentTimeMillis();
+                data.add(new Instance(1.0, vals));
 
                 last_x = x;
                 last_y = y;
