2aba6d5270908b72153255ebc14902283cb15dae
Henneberg <henneberg.anders@gmail.com>
2015-12-03 00:30:19 +0100

    Simplere data-collection og manipulation. Vi samler nu alle målepunkter fra accelerometeret i 1 stor ArrayListe. Derefter kører vi igennem det og bider chunks af 128 målepunkter af. Lidt generel code-cleanup og GUI-cleanup.
    Tror måske ikke udregning af standard-deviation er rigtig. Jeg har oplevet af std deviation er større end forskellen mellem min og max. Det kan vel ikke ske?

--- App or Troll/app/src/main/java/com/example/ander/myapplication/AccelerometerActivity.java
+++ App or Troll/app/src/main/java/com/example/ander/myapplication/AccelerometerActivity.java
@@ -9,16 +9,18 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 import weka.core.Attribute;
 import weka.core.FastVector;
@@ -28,20 +30,25 @@ import weka.core.converters.ArffSaver;
 
 public class AccelerometerActivity extends AppCompatActivity implements SensorEventListener {
 
+    Button butStart, butStop;
+    Button gemData;
+    Spinner spinner;
+    TextView dataField;
+    TextView stepText;
+    private boolean started = false;
+    private int measures = 0;
+
     private SensorManager senSensorManager;
     private Sensor senAccelerometer;
     private long lastUpdate = 0;
-    TextView stepText;
-    private int steps = 0;
-    private int arraySelector = 0;
+    private String selectedMode;
+    private ArrayList<Double> measurements;
+
+    // WEKA variables
     FastVector atts;
     Instances data;
     double[] vals;
-    private ArrayList<Double> array = new ArrayList<Double>();
-    private ArrayList<Double> array2 = new ArrayList<Double>();
-    private ArrayList<Double> temp = new ArrayList<Double>();
-    private int counter = 0;
-    private int arrayCounter = 0;
+
 
 
 
@@ -52,47 +59,106 @@ public class AccelerometerActivity extends AppCompatActivity implements SensorEv
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accelerometer);
 
+        measurements = new ArrayList<Double>();
+        setupWEKAGarbage();
+
         senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
 
-        stepText = (TextView) findViewById(R.id.accelTexr);
-        final TextView dataField = (TextView) findViewById(R.id.dataView);
-        Button dataButton = (Button) findViewById(R.id.dataBotton);
-        dataButton.setOnClickListener(new View.OnClickListener() {
+        stepText = (TextView) findViewById(R.id.tvMeasures);
+        dataField = (TextView) findViewById(R.id.tvData);
+
+        butStart = (Button) findViewById(R.id.butStart);
+        butStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                started = true;
+                butStart.setEnabled(false);
+                butStop.setEnabled(true);
+            }
+        });
+
+
+        butStop = (Button) findViewById(R.id.butStop);
+        butStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                started = false;
+
+                butStart.setEnabled(false);
+                butStop.setEnabled(false);
+                gemData.setEnabled(true);
+
+                doCalculations();
                 dataField.setText(data.toString());
             }
         });
 
-        atts = new FastVector();
-        atts.addElement(new Attribute("minMag"));
-        atts.addElement(new Attribute("maxMag"));
-        atts.addElement(new Attribute("stdMag"));
-        atts.addElement(new Attribute("meanMag"));
-        data = new Instances("Accelerometer", atts, 0);
+        spinner = (Spinner) findViewById(R.id.spinner);
+        final String[] items = new String[]{"MOVEMENT", "Walking", "Running"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0) {
+                    butStart.setEnabled(false);
+                    return;
+                }
+                selectedMode = items[position];
+                butStart.setEnabled(true);
+            }
 
-        Button gemData = (Button) findViewById(R.id.gemData);
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                butStart.setEnabled(false);
+            }
+        });
+
+
+
+        gemData = (Button) findViewById(R.id.gemData);
         gemData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try {
                     ArffSaver saver = new ArffSaver();
                     saver.setInstances(data);
-                    File dest = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "test.arff");
+                    //File dest = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "test.arff");
 
-                    File olddest = new File(Environment
-                            .getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath()
-                            + "/test.arff");
+                    int vs = 0;
+                    File olddest;
+                    while(true) {
+                        olddest = new File(Environment
+                                .getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath()
+                                + "/test"+selectedMode+vs+".arff");
+                        if(olddest.exists()) {
+                            vs++;
+                        } else {
+                            break;
+                        }
+                    }
 
-                    System.out.println("Saved to: " + olddest.getAbsolutePath());
+                    String output = "Saved to: " + olddest.getAbsolutePath();
+
+                    System.out.println(output);
+                    (Toast.makeText(getApplicationContext(), output, Toast.LENGTH_LONG)).show();
+
                     saver.setFile(olddest);
                     saver.writeBatch();
 
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+
+                setupWEKAGarbage();
+                measurements = new ArrayList<Double>();
+                measures = 0;
+                spinner.setSelection(0);
+                gemData.setEnabled(false);
+                dataField.setText("");
+
             }
         });
 
@@ -108,8 +174,28 @@ public class AccelerometerActivity extends AppCompatActivity implements SensorEv
 
     }
 
+    private void doCalculations() {
+        int start = 0;
+        int end = 128;
+        int maxIndex = measurements.size();
+
+        ArrayList<Double> chunk;
+        while((end <= maxIndex)) {
+            chunk = new ArrayList<Double>(measurements.subList(start, end));
+
+            addData(chunk);
+
+            start += 64;
+            end += 64;
+        }
+    }
+
+
+
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
+        if(!started)
+            return;
         Sensor mySensor = sensorEvent.sensor;
 
 
@@ -124,51 +210,61 @@ public class AccelerometerActivity extends AppCompatActivity implements SensorEv
             if ((curTime - lastUpdate) > 200) {
                 lastUpdate = curTime;
 
-                double speed = Math.sqrt(x*x + y*y + z*z );
-                steps++;
-                stepText.setText(""+steps);
+                double speed = Math.sqrt(x*x + y*y + z*z);
+                measures++;
+                stepText.setText(""+ measures);
 
-                array.add(speed);
-                counter++;
+                measurements.add(speed);
 
-                if ((array.size() % 128 )== 0){
-                    saveData(temp);
-                    arrayCounter++;
-                }
 
             }
         }
     }
 
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
-    }
 
-    public void saveData(ArrayList arrayind) {
+    // Calculates the 4 key-numbers for a chunk of 128 accelerometer samples.
+    // Then it adds these numbers to the Instances-object (WEKA-class), which is what we also print to files.
+    public void addData(ArrayList arrayind) {
         ArrayList array = arrayind;
-        vals = new double[data.numAttributes()];
-        double avg = 0;
-        double std = 0;
-        for (int i = 0; i <= 127; i++){
+        double[] vals = new double[data.numAttributes()];
+
+        double avg = 0.0;
+        double std = 0.0;
+        for (int i = 0; i < 128; i++){
             avg = avg + (double) array.get(i);
             System.out.println(avg);
         }
         avg = avg/128;
-        System.out.println(avg);
 
-        for (int i = 0; i <= 127; i++){
+        System.out.println("AVG: "+avg);
+
+        for (int i = 0; i < 128; i++){
             std = Math.pow(((double) array.get(i) - avg) , 2) + std;
             System.out.println(std);
         }
         std = std/128;
-        System.out.println(std);
+        System.out.println("STD: "+std);
 
         vals[0] = (double) Collections.min(array);
         vals[1] = (double) Collections.max(array);
         vals[2] = std;
         vals[3] = avg;
         data.add(new Instance(1.0, vals));
+    }
+
+    private void setupWEKAGarbage() {
+        atts = new FastVector();
+        atts.addElement(new Attribute("minMag"));
+        atts.addElement(new Attribute("maxMag"));
+        atts.addElement(new Attribute("stdMag"));
+        atts.addElement(new Attribute("meanMag"));
+        data = new Instances("Accelerometer", atts, 0);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 
     protected void onPause() {
